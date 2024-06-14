7030f26017157cbb95284137e3114aed652fe584
Jasper <jmc1@kamchen.de>
2024-06-14 20:06:53 +0200

    Test  button removed

--- app/src/main/java/com/pixldev/studyshare/ui/libraryFragment.java
+++ app/src/main/java/com/pixldev/studyshare/ui/libraryFragment.java
@@ -30,16 +30,6 @@ public class libraryFragment extends Fragment {
     private String web_url = "https://www.bundesamtsozialesicherung.de/fileadmin/media/test__pdfs/Test_PDF.pdf";
     private Button button;
 
-    private final ActivityResultLauncher<String> requestPermissionLauncher =
-            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
-                if (isGranted) {
-                    Log.d(TAG, "Permission granted, starting download");
-                    startDownloading();
-                } else {
-                    Log.d(TAG, "Permission denied");
-                    Toast.makeText(getContext(), "Permission denied.....", Toast.LENGTH_SHORT).show();
-                }
-            });
 
     public libraryFragment() {
         // Required empty public constructor
@@ -68,51 +58,8 @@ public class libraryFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_library, container, false);
 
-        button = view.findViewById(R.id.download);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.d(TAG, "Download button clicked");
-                if (ContextCompat.checkSelfPermission(getContext(), android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
-                    Log.d(TAG, "Requesting permissions");
-                    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
-                        if (!Environment.isExternalStorageManager()) {
-                            Intent intent = new Intent(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
-                            startActivity(intent);
-                        } else {
-                            startDownloading();
-                        }
-                    } else {
-                        requestPermissionLauncher.launch(android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
-                    }
-                } else {
-                    Log.d(TAG, "Permissions already granted, starting download");
-                    startDownloading();
-                }
-            }
-        });
-
         return view;
     }
 
-    private void startDownloading() {
-        String url = web_url;
 
-        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
-        request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI | DownloadManager.Request.NETWORK_MOBILE);
-        request.setTitle("Download");
-        request.setDescription("Downloading file.....");
-
-        request.allowScanningByMediaScanner();
-        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "Test_PDF.pdf");
-
-        DownloadManager manager = (DownloadManager) getActivity().getSystemService(Context.DOWNLOAD_SERVICE);
-        if (manager != null) {
-            Log.d(TAG, "Enqueue download request");
-            manager.enqueue(request);
-        } else {
-            Log.e(TAG, "Download manager is null");
-        }
-    }
 }
